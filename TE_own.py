1888f32e6ab245b577f2cb156e8eea7b582b1a1a
Ku4eruk <vasyokkucheruk@gmail.com>
2022-06-04 20:33:37 +0200

    TE_own.py prob_double optimized

--- TE_own.py
+++ TE_own.py
@@ -19,8 +19,8 @@ def S_one(sign: list) -> float:
 # S(y / x) = - SUM(p(x)) * SUM(p(y / x) * log2(p(y / x))) = 
 # = - SUM(p(x, y)* log2(p(y / x)))
 def S_double(sign1: list, sign2: list) -> float: # sign1 - y, sign2 - x
-    joint_probabilities = prob_double(sign1, sign2)
     prob_sign2 = prob_one(sign2)
+    joint_probabilities = prob_double(sign1, sign2, prob_sign2)
     sum_s = 0
     for key1, value1 in joint_probabilities.items():
         for key2, value2 in prob_sign2.items():
@@ -41,9 +41,8 @@ def prob_one(sign: list) -> dict:
 
 
 # p(y / x) joint probability = P(A and B) / P(B) 
-def prob_double(sign1: list, sign2: list) -> dict:
+def prob_double(sign1: list, sign2: list, prob_list_sign2: list) -> dict:
     prob_list_sign1_and_sign2 = prob_one(sign1 + sign2)
-    prob_list_sign2 = prob_one(sign2)
     prob_list_mutual = {}
     for key1, value1 in prob_list_sign1_and_sign2.items():
         prob_list_mutual[key1] = {}
